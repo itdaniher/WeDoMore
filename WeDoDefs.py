5be99a02f164b2f5478807d6786586120c56c8eb
Ian Daniher <it.daniher@gmail.com>
2011-02-13 02:16:28 -0500

    added scaling for tilt sensor

--- WeDoDefs.py
+++ WeDoDefs.py
@@ -2,3 +2,16 @@ tiltID = 2
 distanceID = 9
 lightID = 10
 motorID = 13
+#motor takes a signed 8-bit number
+
+def processTilt(v):
+	if v < 49:
+		return 3
+	elif v < 100:
+		return 2
+	elif v < 154:
+		return 0
+	elif v < 205:
+		return 1
+	else:
+		return 0
