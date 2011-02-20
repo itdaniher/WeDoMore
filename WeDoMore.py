399cc6b0d9ecbc92f2cffc4d79c591dcf3ea7d13
Ian Daniher <it.daniher@gmail.com>
2011-02-19 22:08:47 -0500

    exploring various solutions for this weird off-by-one-LSB-error.

--- WeDoMore.py
+++ WeDoMore.py
@@ -17,9 +17,14 @@ if WeDo.is_kernel_driver_active(0):
 	except usb.core.USBError as e:
 		sys.exit("Could not detatch kernel driver: %s" % str(e))
 
-endpoint = WeDo[0][(0,0)][0]
-
-print(endpoint.read(64)[-8:])
+def WeDoGet():
+	"""Use recursion to clean out the ±1 LSB offset issue."""
+	endpoint = WeDo[0][(0,0)][0]
+	data = list(endpoint.read(64)[-8:])
+	if data[5] != 231:
+		return data
+	else:
+		return WeDoGet()
 
 def WeDoWrite(motorA, motorB):
 	"""Arguments should be in form of a number between 0 and 127, positive or negative. Magic numbers used for the ctrl_transfer derived from sniffing USB coms."""
@@ -28,3 +33,14 @@ def WeDoWrite(motorA, motorB):
 	magicNumber = 64
 	data = [magicNumber, motorA&0xFF, motorB&0xFF, 0x00, 0x00, 0x00, 0x00, 0x00]
 	WeDo.ctrl_transfer(bmRequestType = 0x21, bRequest = 0x09, wValue = 0x0200, wIndex = 0, data_or_wLength = data)
+
+lshift = lambda x: x >> 1
+
+def getData():
+	rawData = map(lshift, WeDoGet())
+	sensorData = {rawData[3]: rawData[2], rawData[5]: rawData[4]}
+	return (rawData, sensorData)
+
+distanceSensorIDS = [88, 89]
+
+
