90ebaf8f1e5c77742ae70522996a8815ccfb0189
Guillaume BINET <gbin@gootz.net>
2013-03-30 22:37:44 -0700

    converted everything as rst as it is what pypi supports

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,8 @@
-# CHANGELOG
+CHANGELOG
+=========
 
-## 2013-04-30 v1.0.0
+2013-04-30 v1.0.0
+-----------------
 
 - massive refactoring and packaging for the first version
 - basic documentation
--- setup.py
+++ setup.py
@@ -7,35 +7,27 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-src_root = "wedo"
-if __name__ == "__main__":
-
-    setup(
-        name="wedo",
-        version="1.0.0",
-        packages=find_packages(src_root),
-        install_requires=['pyusb'],
-
-        package_data={
-            '': ['*.txt', '*.md', '*.plug', '*.html', '*.js', '*.css'],
-        },
-
-        author="Guillaume BINET",
-        author_email="gbin@gootz.net",
-        description="This is a python library for the Lego WeDo, a tethered-over-USB sensing and robotics toolkit produced by Lego for the educational market.",
-        long_description=''.join([read('README.md'), '\n\n', read('CHANGES.md')]),
-        license="GPL",
-        keywords="lego wedo motor tilt sensor driver",
-        url="https://github.com/gbin/WeDoMore",
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Intended Audience :: Education",
-            "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-            "Operating System :: Linux",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.2",
-        ],
-        src_root=src_root,
-    )
+setup(
+    name='wedo',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['pyusb'],
+    zip_safe=False,
+    include_package_data=True,
+    author="Guillaume BINET",
+    author_email="gbin@gootz.net",
+    description="This is a python library for the Lego WeDo, a tethered-over-USB sensing and robotics toolkit produced by Lego for the educational market.",
+    long_description=''.join([read('README.md'), '\n\n', read('CHANGES.rst')]),
+    license="GPL",
+    keywords="lego wedo motor tilt sensor driver",
+    url="https://github.com/gbin/WeDoMore",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2"]
+)
