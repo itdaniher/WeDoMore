4b14bf9bab5feecd9a4d792dd1d50b043dbfaff0
Guillaume BINET <gbin@gootz.net>
2013-03-30 21:27:46 -0700

    remove the header as I put a correct license file + contributors

--- wedo/__init__.py
+++ wedo/__init__.py
@@ -1,19 +1,3 @@
-#Copyright (c) 2011, 2012, Ian Daniher
-#Copyright (c) 2012, Tony Forster, Walter Bender
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 from functools import wraps
 
 from wedo.distance import interpolate_distance_data
@@ -34,6 +18,7 @@ MOTOR = (0, 1, 2, 3, 238, 239)
 
 # limit the visibility to simplify the usage
 __all__ = ["scan_for_devices", "WeDo", "FLAT", "TILT_BACK", "TILT_FORWARD", "TILT_LEFT", "TILT_RIGHT"]
+
 
 def device_required(f):
     """ A simple decorator to protect the instances with non working devices.
